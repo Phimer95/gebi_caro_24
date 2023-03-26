8edcc087ef97ce13001435a5e877c5797bc90b9f
philipp <merk.ph02@gmail.com>
2023-03-26 01:11:17 +0100

    fertig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,15 +16,15 @@ const gifts = [
     text: "Am zweiten Tag beschlossen sie, eine Mountainbike-Tour durch die wunderschönen Hügel rund um ihre Stadt zu unternehmen. Sie stiegen auf ihre speziell angefertigten katzen-großen Fahrräder und fuhren den Weg hinauf, ihr Fell wehte im Wind. Sie bestaunten die üppigen Wälder, funkelnden Bäche und atemberaubenden Ausblicke, die sie auf ihrer Reise entdeckten. Als die Sonne zu sinken begann, hielten sie inne, um einen zärtlichen Moment miteinander zu teilen und die Schönheit der Welt zu bewundern.",
     date: "08.04.2023",
     image:
-      "https://cdn.discordapp.com/attachments/1008571088919343124/1089323633048031312/Philipp_Merk_2_cats_doing_a_bike_tour_in_the_mountains_a3cd58ae-75a6-42c4-ba12-e8e3fa2ef05a.png",
+      "https://cdn.discordapp.com/attachments/1008571088919343124/1089337279077687337/Philipp_Merk_2_cats_mountain_biking_fantasy_drawn_6d43f750-47f7-49f4-b641-f4afc23ede90.png",
   },
   {
     id: 3,
     title: "Ein Tag im Spa",
-    text: "Am dritten Tag beschlossen sie, einen Tag im Spa zu verbringen. Sie ließen sich von den sanften Wellen des Wassers in den Whirlpools und der Wärme verwöhnen. Sie genossen die Ruhe und Entspannung, die sie in diesem Moment erlebten. Als sie sich wieder auf den Weg machten, fühlten sie sich wie neu gebohrene Kadsen.",
+    text: "Am dritten Tag beschlossen sie, einen Tag im Thermalbad zu verbringen. Sie ließen sich von den sanften Wellen des Wassers in den Whirlpools und der Wärme verwöhnen. Sie genossen die Ruhe und Entspannung, die sie in diesem Moment erlebten. Als sie sich wieder auf den Weg machten, fühlten sie sich wie neu gebohrene Kadsen.",
     date: "09.04.2023",
     image:
-      "https://cdn.discordapp.com/ephemeral-attachments/1008571088919343124/1089325876317003798/Philipp_Merk_2_cats_in_a_thermal_bath_718dde1d-74d8-448c-b0bb-630a7dbb7833.png",
+      "https://cdn.discordapp.com/ephemeral-attachments/1008571088919343124/1089338769829810216/Philipp_Merk_2_cats_in_a_thermal_bath_futuristic_romantic_drawn_8cf60db9-b7ae-4aa7-9cb6-d4e28df1089c.png",
   },
   {
     id: 4,
@@ -32,15 +32,15 @@ const gifts = [
     text: "An einem weiteren Tag kleideten sich Süßvieh und Großkadse in ihre feinste Garderobe und begaben sich zu einem exquisiten Restaurant, das ein besonderes Dinner für zwei anbot. Das warme Kerzenlicht und die sanfte Musik erfüllten die Luft, während sie an einem privaten Tisch am Fenster Platz nahmen. Sie genossen jeden Bissen ihres Gourmet-Mahls und tauschten liebevolle Blicke aus, während sie speisten.",
     date: "10.04.2023",
     image:
-      "https://cdn.discordapp.com/attachments/1008571088919343124/1089324542138273832/Philipp_Merk_2_Cats_going_to_a_romantic_dinner_97b1c8e8-6400-46b2-9558-93c5cae63ccf.png",
+      "https://cdn.discordapp.com/attachments/1008571088919343124/1089336385808384052/Philipp_Merk_2_cats_romantic_dinner_fantasy_cc41b79b-6026-4772-af02-e5fddcb32bca.png",
   },
   {
     id: 5,
     title: "Bowling",
-    text: "Schließlich, am letzten Tag ihres Abenteuers wollten sie sehen wie ehrenvollere der beiden Kadsen war. In einem Kopf-an-Kopf-Rennen, das bis zum letzten Moment andauerte, konnte Süßvieh sich schließlich durchsetzen und gewann das Spiel.",
+    text: "Schließlich, am letzten Tag ihres Abenteuers wollten sie sehen wer die Geschicklichere der beiden Kadsen war. In einem Kopf-an-Kopf-Rennen, das bis zum letzten Moment andauerte, konnte Süßvieh sich schließlich durchsetzen und gewann das Spiel.",
     date: "11.04.2023",
     image:
-      "https://cdn.discordapp.com/attachments/1008571088919343124/1089329104106569848/Philipp_Merk_cinema_romantic_2_cats_aadd8467-89fb-425e-850e-4fa019ea4519.png",
+      "https://cdn.discordapp.com/attachments/1008571088919343124/1089339878795378768/Philipp_Merk_2_cats_bowling_drawn_futuristic_98b6c387-e626-40dd-92b0-de07c192aea0.png",
   },
 ];
 
@@ -55,7 +55,7 @@ export default function Home() {
       {gifts.map((gift) => (
         <SwiperSlide key={gift.id}>
           <div className="relative h-screen w-screen text-white">
-            <section className="z-50 max-w-3xl w-full absolute left-3/4 top-3/4 -translate-x-1/2 -translate-y-1/2 text-justify bg-slate-800/80 p-5 rounded-3xl shadow-md">
+            <section className="z-50 max-w-3xl w-full absolute left-3/4 top-3/4 -translate-x-1/2 -translate-y-1/2 text-justify bg-slate-800/50 p-5 rounded-3xl shadow-md">
               <h1 className="text-4xl font-bold z-50 mb-6">{gift.title}</h1>
               <p className="text-lg z-50 font-semibold">{gift.text}</p>
               <p className="flex mt-4">
@@ -74,6 +74,7 @@ export default function Home() {
                 width: "100%",
                 height: "100%",
                 objectFit: "cover",
+                objectPosition: "center",
               }}
               src={gift.image}
             />
